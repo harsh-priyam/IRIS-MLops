9cbcb935b5314959f42806a28f41e834e8e1febe
harsh <harshpriyam.official@gmail.com>
2024-01-13 01:46:43 +0530

    Data Transformation Stage Added & DvC Updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from IRIS_MLops import logger
 
 from IRIS_MLops.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from IRIS_MLops.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from IRIS_MLops.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -21,6 +22,18 @@ STAGE_NAME = "Data Validation stage"
 try:
         logger.info(f">>>>> stage {STAGE_NAME} started")
         obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started")
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
 except Exception as e:
