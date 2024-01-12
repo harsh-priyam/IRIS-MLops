a7bfa15e3a5de16c37a3ed280fa20740ff8d67b6
harsh <harshpriyam.official@gmail.com>
2024-01-13 00:02:30 +0530

    Data Ingestion Stage Created

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from IRIS_MLops import logger
 
-logger.info("Welcome to the custom log")
+from IRIS_MLops.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
